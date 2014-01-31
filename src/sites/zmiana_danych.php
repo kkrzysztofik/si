80ac6b68ec1112a7cdf1d475a45b6bb1908828f2
Krzysztof Krzysztofik <krzysztof.krzysztofik@teonite.com>
2014-01-31 03:15:48 +0100

    lab7 w-i-p

---
+++ src/sites/zmiana_danych.php
@@ -0,0 +1,142 @@
+<?php
+include_once("inc/common.php");
+
+$is_permitted = check_permission(1);
+
+if(!$is_permitted) {
+    die('Permission denited');
+}
+
+$error = false;
+$error_string = "";
+
+$elements = array('imie' => 'Imię', 'nazwisko' => 'Nazwisko', 'login' => 'Login', 'haslo' => 'Hasło',
+    'powt_haslo' => 'Powtórne hasło');
+
+function check_if_exists($elements_array) {
+    global $error_string;
+    $error = false;
+
+    foreach($elements_array as $key => $value){
+        if(empty($_POST[$key]) && !isset($_POST[$key])){
+            $error_string = $error_string . $value . ' nie jest podany <br>';
+            $error = true;
+        }
+    }
+
+    return $error;
+}
+
+$SQL = "SELECT * FROM user WHERE id='".$_SESSION['user_id']."' LIMIT 1;";
+$rows = mysql_query($SQL);
+if (!$rows) {
+    die('Invalid query: ' . mysql_error());
+}
+
+$row = mysql_fetch_assoc($rows);
+//var_dump($row);
+if (!$row) {
+    header('Location: /index.php');
+}
+
+if(isset($_POST['wyslij'])) {
+    $form_array = array('imie' => sanitize_string($_POST['imie']), 'nazwisko' => sanitize_string($_POST['nazwisko']),
+        'haslo' => sanitize_string($_POST['haslo']), 'login' => sanitize_string($_POST['login']),
+        'powt_haslo' => sanitize_string($_POST['powt_haslo'])
+    );
+
+    $error = check_if_exists($elements);
+
+    if($form_array['haslo'] != $form_array['powt_haslo']) {
+        $error_string = $error_string . 'Podane hasła się nie zgadzają <br>';
+        $error = true;
+    }
+    if(strlen($form_array['login']) < 6) {
+        $error_string = $error_string . 'Login jest za krótki, wymagane min. 6 znaków<br>';
+        $error = true;
+    }
+    if(strlen($form_array['haslo']) < 6) {
+        $error_string = $error_string . 'Hasło jest za krótkie, wymagane min. 6 znaków<br>';
+        $error = true;
+    }
+
+    //check if login is unique
+    $SQL = "SELECT * FROM user WHERE login='".$form_array['login']."';";
+
+    $wynik1 = mysql_query($SQL);
+    if (!$wynik1) {
+        die('Invalid query: ' . mysql_error());
+    }
+    $num1 = mysql_num_rows($wynik1);
+
+    if($num1) {
+        $error_string = $error_string . 'Login zajęty<br>';
+        $error = true;
+    }
+
+} else {
+    $error = true;
+}
+
+if($error){
+    ?>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td><label for="imie">Imię:</label></td>
+                <td>
+                    <input id="imie" type="text" name="imie" value="<?php if(isset($_POST['imie'])){ echo sanitize_string($_POST['imie']);}
+                    else { echo $row['imie']; } ?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="nazwisko">Nazwisko:</label></td>
+                <td>
+                    <input id="nazwisko" type="text" name="nazwisko" value="<?php if(isset($_POST['nazwisko'])){ echo sanitize_string($_POST['nazwisko']);}
+                    else { echo $row['nazwisko']; }?>"/>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="login">Login:</label></td>
+                <td>
+                    <input id="login" type="text" name="login" value="<?php if(isset($_POST['login'])){ echo sanitize_string($_POST['login']);}
+                    else { echo $row['login']; } ?>" />
+                </td>
+            </tr>
+            <tr>
+                <td><label for="haslo">Hasło:</label></td>
+                <td>
+                    <input id="haslo" type="password" name="haslo" value="<?php if(isset($_POST['haslo'])){ echo sanitize_string($_POST['haslo']);} ?>" />
+                </td>
+            </tr>
+            <tr>
+                <td><label for="powt_haslo">Powtórz hasło:</label></td>
+                <td>
+                    <input id="powt_haslo" type="password" name="powt_haslo" value="<?php if(isset($_POST['powt_haslo'])){ echo sanitize_string($_POST['powt_haslo']);} ?>" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input name="wyslij" type="submit" value="Potwierdź zmiany" />
+                    <button type="button"><a href="/index.php?site=5">Odrzuć zmiany</a></button>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <?php
+    echo $error_string;
+}
+else {
+    $form_array = array('imie' => sanitize_string($_POST['imie']), 'nazwisko' => sanitize_string($_POST['nazwisko']),
+        'haslo' => sanitize_string($_POST['haslo']), 'login' => sanitize_string($_POST['login'])
+    );
+
+    $sql = "UPDATE user SET imie='".$form_array["imie"]."', nazwisko='".$form_array["nazwisko"]."',
+    haslo='".md5($form_array['haslo'])."', login='".$form_array["login"]."' WHERE id=".$_SESSION['user_id'].";";
+    //echo $sql;
+    $result = mysql_query($sql);
+    if (!$result) {
+        die('Invalid query: ' . mysql_error());
+    }
+    echo 'Zaktualizowano użytkownika '.$form_array['imie'].' '.$form_array['nazwisko'];
+}
---
+++ src/sites/zmiana_poziomu.php
@@ -0,0 +1,128 @@
+<?php
+
+include_once('inc/common.php');
+
+$is_permitted = check_permission(4);
+
+if(!$is_permitted) {
+    die('Permission denited');
+}
+
+if(isset($_POST['wyslij'])) {
+    $id = (int) $_POST['id'];
+
+    if($id == $_SESSION['user_id']){
+        die('Nie możesz zmienić sobie uprawnień.');
+    }
+
+    $uprawnienia = (int) $_POST['uprawnienia'];
+
+    $sql = "UPDATE user SET uprawnienia='".$uprawnienia."' WHERE ID='".$id."';";
+
+    //echo $sql;
+    $result = mysql_query($sql);
+    if (!$result || $result != 1) {
+        die('Invalid query: ' . mysql_error());
+    }
+    header('Location: /index.php?site=12');
+}
+
+if (!empty($_GET['page']) && isset($_GET['page']) && is_numeric($_GET['page'])) {
+    $page = (int) $_GET['page'];
+    if ($page < 1) {
+        $page = 1;
+    }
+}
+else {
+    $page = 1;
+}
+
+$start = ($page - 1) * $employees_perPage;
+
+$SQL = 'SELECT COUNT(*) FROM user;';
+$wynik1 = mysql_query($SQL);
+if (!$wynik1) {
+    die('Invalid query: ' . mysql_error());
+}
+$num1 = mysql_result($wynik1, 0, 'Count(*)');
+
+$SQL = 'SELECT COUNT(*) FROM user ORDER BY id asc LIMIT '.$start.','.$employees_perPage.';';
+$wynik = mysql_query($SQL);
+if (!$wynik) {
+    die('Invalid query: ' . mysql_error());
+}
+$num = mysql_result($wynik, 0, 'Count(*)');
+
+$max=ceil($num1/$employees_perPage);
+
+$SQL = 'SELECT * FROM user ORDER BY id asc LIMIT '.$start.','.$employees_perPage.';';
+$rows = mysql_query($SQL);
+if (!$rows) {
+    die('Invalid query: ' . mysql_error());
+}
+?>
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Imię</td>
+        <td>Nazwisko</td>
+        <td>Login</td>
+        <td>Poziom</td>
+        <td>&nbsp;</td>
+    </tr>
+    <?php
+    while ($result = mysql_fetch_assoc($rows)) {
+        $uprawnienia = (int) $result['uprawnienia'];
+        ?>
+        <form action="" method="post" id="form_<?php echo sanitize_string($result['id']); ?>">
+            <tr>
+                <input name="id" id="id" type="hidden" value="<?php echo sanitize_string($result['id']); ?>">
+                <td> <?php echo sanitize_string($result['id']); ?> </td>
+                <td> <?php echo sanitize_string($result['imie']); ?></td>
+                <td> <?php echo sanitize_string($result['nazwisko']); ?></td>
+                <td> <?php echo sanitize_string($result['login']); ?></td>
+                <td><select name="uprawnienia" form="form_<?php echo sanitize_string($result['id']); ?>">
+                    <option value="0" <?php if($uprawnienia == 0){ echo 'selected'; } ?>>0</option>
+                    <option value="1" <?php if($uprawnienia == 1){ echo 'selected'; } ?>>1</option>
+                    <option value="2" <?php if($uprawnienia == 2){ echo 'selected'; } ?>>2</option>
+                    <option value="3" <?php if($uprawnienia == 3){ echo 'selected'; } ?>>3</option>
+                    <option value="4" <?php if($uprawnienia == 4){ echo 'selected'; } ?>>4</option>
+                </select>
+                </td>
+                <td><input name="wyslij" type="submit" value="Potwierdź zmiany" /></td>
+            </tr>
+        </form>
+    <?php
+    }
+    ?>
+</table>
+<div id="paging">
+    <?php
+    /////////////////////////////////////////////////////////////
+    $prev = $page - 1;
+    $next = $page + 1;
+
+    ///..................numery stron
+    if($prev > 0) {
+        echo '<a href="/index.php?site=12&page='.$prev.'"><-</a>';
+    } else {
+        echo '<-';
+    }
+
+    for($i=0; $i<$max; $i++)
+    {
+        $tmp = $i + 1;
+        echo '<a href="/index.php?site=12&page='.$tmp.'">'.$tmp.'</a>';
+        if(($i!=$max)-1)
+            echo" |";
+
+    }
+
+    if($next<$max){
+        echo '<a href="/index.php?site=12&page='.$next.'">-></a>';
+    } else {
+        echo '->';
+    }
+
+    ?>
+</div>
